2b06149dd4d3777334bdfaf894860b9eb51ff0f9
AmirMohammad <amirmohamadm.y5@gmail.com>
2024-02-17 16:29:12 +0330

    Add GroupingContext

--- src/app/categories/page.jsx
+++ src/app/categories/page.jsx
@@ -1,65 +1,18 @@
+"use client";
+
 import CategoryComponent from "@/components/home/CategoryComponent";
+import { GroupingContext, useGrouping } from "@/context/GroupingContext";
+import { useContext } from "react";
 import { Grid } from "@mui/material";
 import React from "react";
 
-const categories = [
-  {
-    name: "لبنیات",
-    src: "/img/home/category-labaniat.jpg",
-    href: "labaniat",
-  },
-  {
-    name: "تنقلات",
-    src: "/img/home/category-labaniat.jpg",
-    href: "tanagholat",
-  },
-  {
-    name: "حبوبات",
-    src: "/img/home/category-labaniat.jpg",
-    href: "hobobat",
-  },
-  {
-    name: "نوشیدنی",
-    src: "/img/home/category-labaniat.jpg",
-    href: "noshidani",
-  },
-  {
-    name: "کیک و بیسکوویت",
-    src: "/img/home/category-labaniat.jpg",
-    href: "cake",
-  },
-  {
-    name: "کیک و بیسکوویت",
-    src: "/img/home/category-labaniat.jpg",
-    href: "cake",
-  },
-  {
-    name: "کیک و بیسکوویت",
-    src: "/img/home/category-labaniat.jpg",
-    href: "cake",
-  },
-  {
-    name: "کیک و بیسکوویت",
-    src: "/img/home/category-labaniat.jpg",
-    href: "cake",
-  },
-  {
-    name: "کیک و بیسکوویت",
-    src: "/img/home/category-labaniat.jpg",
-    href: "cake",
-  },
-  {
-    name: "کیک و بیسکوویت",
-    src: "/img/home/category-labaniat.jpg",
-    href: "cake",
-  },
-];
-
 export default function Categories() {
+  //const grouping = useGrouping(); ?
+  const grouping = useContext(GroupingContext);
   return (
     <>
       <Grid container gap={3} justifyContent="center">
-        {categories.map((item) => (
+        {grouping.map((item) => (
           <Grid item xs={3} lg={2}>
             <CategoryComponent
               href={item.href}
--- src/components/home/Categories.jsx
+++ src/components/home/Categories.jsx
@@ -1,66 +1,64 @@
-import { Grid, Typography } from "@mui/material"
-import CategoryComponent from "./CategoryComponent"
-import { TbCategory } from "react-icons/tb"
+"use client";
+
+import { Grid, Typography } from "@mui/material";
+import CategoryComponent from "./CategoryComponent";
+import { TbCategory } from "react-icons/tb";
+import { GroupingContext, useGrouping } from "@/context/GroupingContext";
+import { useContext } from "react";
 
 export default function Categories() {
-    return (
-        <div className="m-4">
+  //const grouping = useGrouping(); ?
+  const grouping = useContext(GroupingContext);
+  return (
+    <div className="m-4">
+      <div
+        className="border border-gray-300 top-3 relative"
+        style={{
+          transform: "translateY(15px)",
+          borderTopWidth: "2px",
+          borderTopRightRadius: "50%",
+          borderTopLeftRadius: "50%",
+        }}
+      ></div>
 
-            <div className='border border-gray-300 top-3 relative' style={{ transform: 'translateY(15px)', borderTopWidth: '2px', borderTopRightRadius: '50%', borderTopLeftRadius: '50%' }}></div>
+      <div>
+        <Typography
+          sx={{
+            zIndex: "100",
+            position: "relative",
+            width: {
+              sm: "20%",
+              md: "22%",
+              lg: "20%",
+              xl: "15%",
+            },
+            fontSize: {
+              xs: "20px",
+              sm: "16px",
+              md: "20px",
+              lg: "24px",
+              xl: "30px",
+            },
+          }}
+          className="bg-white m-2 flex justify-center mx-auto"
+        >
+          <TbCategory className="mt-1 text-cyan-500" />
+          <span className="mx-2">دسته بندی</span>
+        </Typography>
+      </div>
 
-            <div>
-                <Typography
-                    sx={{
-                        zIndex: '100',
-                        position: 'relative',
-                        width: {
-                            sm: '20%',
-                            md: '22%',
-                            lg: '20%',
-                            xl: '15%'
-                        },
-                        fontSize: {
-                            xs: '20px',
-                            sm: '16px',
-                            md: '20px',
-                            lg: '24px',
-                            xl: '30px'
-                        }
-                    }}
-                    className='bg-white m-2 flex justify-center mx-auto'>
-                    <TbCategory className='mt-1 text-cyan-500' />
-                    <span className='mx-2'>دسته بندی</span>
-                </Typography>
-            </div>
-
-
-            <Grid container gap={2} justifyContent={"center"}>
-                <Grid item xs={3} lg={2}>
-                    <CategoryComponent href='/#' caption='لبنیات' src='/img/home/category-labaniat.jpg' charachtersLengthLevel={0} />
-                </Grid>
-                <Grid item xs={3} lg={2}>
-                    <CategoryComponent href='/#' caption='تنقلات' src='/img/home/tanagholat.jpg' charachtersLengthLevel={0} />
-                </Grid>
-                <Grid item xs={3} lg={2}>
-                    <CategoryComponent href='/#' caption='نوشیدنی' src='/img/home/category-labaniat.jpg' charachtersLengthLevel={0} />
-                </Grid>
-                <Grid item xs={3} lg={2}>
-                    <CategoryComponent href='/#' caption='صبحانه' src='/img/home/tanagholat.jpg' charachtersLengthLevel={0} />
-                </Grid>
-                <Grid item xs={3} lg={2}>
-                    <CategoryComponent href='/#' caption='چاشنی و افزودنی' src='/img/home/category-labaniat.jpg' charachtersLengthLevel={1} />
-                </Grid>
-                <Grid item xs={3} lg={2}>
-                    <CategoryComponent href='/#' caption='خواربار' src='/img/home/tanagholat.jpg' charachtersLengthLevel={0} />
-                </Grid>
-                <Grid item xs={3} lg={2}>
-                    <CategoryComponent href='/#' caption='آرایشی بهداشتی' src='/img/home/category-labaniat.jpg' charachtersLengthLevel={1} />
-                </Grid>
-                <Grid item xs={3} lg={2}>
-                    <CategoryComponent href='/#' caption='شوینده و دستمال' src='/img/home/tanagholat.jpg' charachtersLengthLevel={2} />
-                </Grid>
-            </Grid>
-
-        </div>
-    )
+      <Grid container gap={2} justifyContent="center">
+        {grouping.map((item) => (
+          <Grid item xs={3} lg={2}>
+            <CategoryComponent
+              href={item.href}
+              caption={item.name}
+              src={item.src}
+              charachtersLengthLevel={0}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
 }
