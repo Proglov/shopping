ca781d64cdde9d100c676269c7b511bc8d63f635
AmirMohammad <amirmohamadm.y5@gmail.com>
2024-06-02 13:58:34 +0330

    Some changes

--- src/components/users/signup/SignUpComponent.jsx
+++ src/components/users/signup/SignUpComponent.jsx
@@ -10,26 +10,44 @@ import { SetLogin } from "@/features/Login/LoginSlice";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import DOMPurify from "dompurify";
+import { Password } from "@mui/icons-material";
 
 export default function SignUpComponent() {
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [show, setShow] = useState([false, false]);
+  const [information, setInformation] = useState({
+    phone: "",
+    username: "",
+    password: "",
+  });
+  const [repeatPass, setRepeatPass] = useState("");
+  const [show, setShow] = useState([false, false, false]);
   const { signUp } = UserApi;
   const router = useRouter();
   const dispatch = useAppDispatch();
 
   const submit = async () => {
-    if (!phoneNumber) {
-      setShow([true, false]);
+    if (
+      (information.password === "") |
+      (information.phone === "") |
+      (information.username === "") |
+      (repeatPass === "")
+    ) {
+      setShow([true, false, false]);
       return;
     }
-    if (!/^[0][9]([0-9]{9})$/.test(phoneNumber)) {
-      setShow([false, true]);
+    if (!/^[0][9]([0-9]{9})$/.test(information.phone)) {
+      setShow([false, true, false]);
+      return;
+    }
+    if (repeatPass !== information.password) {
+      setShow([false, false, true]);
       return;
     }
     try {
-      const phone = DOMPurify.sanitize(phoneNumber);
-      const response = await signUp({ phone: phone });
+      const obj = information;
+      obj.username = DOMPurify.sanitize(obj.username);
+      obj.password = DOMPurify.sanitize(obj.password);
+      obj.phone = DOMPurify.sanitize(obj.phone);
+      const response = await signUp(obj);
       localStorage.setItem("token", response.token);
       toast.success("ثبت نام شما موفقیت آمیز بود", {
         position: toast.POSITION.TOP_RIGHT,
@@ -41,6 +59,21 @@ export default function SignUpComponent() {
       const mes = error.response.data.message;
       if (mes === "phone number is invalid!") {
         toast.error("شماره تلفن نامعتبر است", {
+          position: toast.POSITION.TOP_RIGHT,
+        });
+      }
+      if (mes === "Password Should have more than 8 characters") {
+        toast.error("رمز عبور باید حداقل شامل هشت کارکتر باشد", {
+          position: toast.POSITION.TOP_RIGHT,
+        });
+      }
+      if (mes === "username is not valid") {
+        toast.error("نام کاربری نامعتبر است", {
+          position: toast.POSITION.TOP_RIGHT,
+        });
+      }
+      if (mes === "Username Already Exists") {
+        toast.error("این نام کاربری قبلا استفاده شده است", {
           position: toast.POSITION.TOP_RIGHT,
         });
       }
@@ -59,10 +92,10 @@ export default function SignUpComponent() {
 
   return (
     <>
-      <Box component="div" className="grid place-items-center h-full w-full">
+      <Box component="div" className="grid place-items-center w-full">
         <Box
           component="div"
-          className="w-full xl:w-1/3 md:w-2/3 h-full md:h-4/5 p-10 border border-slate-300 bg-white rounded-lg"
+          className="w-full md:w-2/3 h-fit my-10 p-10 border border-slate-300 bg-white rounded-lg"
         >
           <Box className="mb-6 text-2xl text-center" component="div">
             فروشگاه آنلاین
@@ -73,11 +106,11 @@ export default function SignUpComponent() {
           <Box className="text-base mb-6 font-thin" component="div">
             سلام!
             <br />
-            لطفا شماره موبایل خود را وارد کنید
+            لطفا اطلاعات خواسته شده را وارد کنید
           </Box>
-          <Box className="mb-4">
+          <Box className="mb-4 grid md:grid-cols-2 grid-cols-1 gap-4">
             <TextField
-              value={phoneNumber}
+              value={information.phone}
               fullWidth
               label="شماره موبایل"
               onKeyDown={(event) => {
@@ -88,7 +121,78 @@ export default function SignUpComponent() {
               onChange={(event) => {
                 const input = event.target.value;
                 const filteredInput = input.replace(/\D/g, "");
-                setPhoneNumber(filteredInput);
+                setInformation({ ...information, phone: filteredInput });
+              }}
+              sx={{
+                " & .MuiInputLabel-root": {
+                  left: "inherit !important",
+                  right: "1.75rem !important",
+                  transformOrigin: "right !important",
+                },
+                "& legend": { textAlign: "right" },
+              }}
+            />
+            <TextField
+              value={information.username}
+              fullWidth
+              label="نام کاربری"
+              onKeyDown={(event) => {
+                if (event.key === "Enter") {
+                  submit();
+                }
+              }}
+              onChange={(event) => {
+                setInformation({
+                  ...information,
+                  username: event.target.valueZ,
+                });
+              }}
+              sx={{
+                " & .MuiInputLabel-root": {
+                  left: "inherit !important",
+                  right: "1.75rem !important",
+                  transformOrigin: "right !important",
+                },
+                "& legend": { textAlign: "right" },
+              }}
+            />
+            <TextField
+              value={information.password}
+              fullWidth
+              label="رمز عبور"
+              type="password"
+              onKeyDown={(event) => {
+                if (event.key === "Enter") {
+                  submit();
+                }
+              }}
+              onChange={(event) => {
+                setInformation({
+                  ...information,
+                  password: event.target.valueZ,
+                });
+              }}
+              sx={{
+                " & .MuiInputLabel-root": {
+                  left: "inherit !important",
+                  right: "1.75rem !important",
+                  transformOrigin: "right !important",
+                },
+                "& legend": { textAlign: "right" },
+              }}
+            />
+            <TextField
+              value={repeatPass}
+              fullWidth
+              label="تکرار رمز عبور"
+              type="password"
+              onKeyDown={(event) => {
+                if (event.key === "Enter") {
+                  submit();
+                }
+              }}
+              onChange={(event) => {
+                setRepeatPass(event.target.value);
               }}
               sx={{
                 " & .MuiInputLabel-root": {
@@ -100,18 +204,29 @@ export default function SignUpComponent() {
               }}
             />
           </Box>
-          {show[0] ? (
-            <span className="text-red-600">لطفا این قسمت را خالی نگذارید!</span>
-          ) : (
-            ""
-          )}
-          {show[1] ? (
-            <span className="text-red-600">
-              شماره موبایل به درستی وارد نشده است!
-            </span>
-          ) : (
-            ""
-          )}
+          <Box className="mt-3 text-center">
+            {show[0] ? (
+              <span className="text-red-600">
+                لطفا تمامی موارد خواسته شده را وارد کنید !
+              </span>
+            ) : (
+              ""
+            )}
+            {show[1] ? (
+              <span className="text-red-600">
+                شماره موبایل به درستی وارد نشده است !
+              </span>
+            ) : (
+              ""
+            )}
+            {show[2] ? (
+              <span className="text-red-600">
+                رمز عبور با تکرار آن مطابقت ندارد !
+              </span>
+            ) : (
+              ""
+            )}
+          </Box>
           <Button
             variant="contained"
             className="bg-green-500 hover:bg-green-600 text-xl rounded-lg w-full mt-10"
