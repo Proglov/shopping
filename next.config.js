f1e23dbcaf1799f2b4c338fda441d95886a59bf0
Proglov <dev.mohammad.parvizi@gmail.com>
2024-06-21 17:23:17 +0330

    updated next config

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
                 port: '3000',
             },
             {
-                hostname: 'shop-storage.storage.iran.liara.space'
+                hostname: 'shop-image.storage.c2.liara.space'
             }
         ],
     },
