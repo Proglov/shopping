44edf10278c9a693d2713c2159bebe4104853358
Proglov <dev.mohammad.parvizi@gmail.com>
2024-10-21 10:18:07 +0330

    improved the ui in CardItem

--- src/components/categorization/CardItem.jsx
+++ src/components/categorization/CardItem.jsx
@@ -6,10 +6,8 @@ import AddButton from "../AddButton";
 
 export default function CardItem({ product, subID, id, margin = 1 }) {
   let img;
-  if (product?.imagesUrl?.length !== 0) {
-    img = product.imagesUrl[0]
-  } else
-    img = "/img/no-pic.png"
+  if (product?.imagesUrl?.length !== 0) img = product.imagesUrl[0]
+  else img = "/img/no-pic.png"
 
   return (
     <Card className={`relative mx-${margin} sm:max-w-[200px] max-w-[180px] min-h-[400px] p-1 mb-2 shadow-lg`} sx={{ border: '1px solid #dbd9d9' }}>
@@ -18,57 +16,61 @@ export default function CardItem({ product, subID, id, margin = 1 }) {
         component="img"
         image={img}
       />
-      <CardContent>
+      <CardContent className="min-h-[300px]">
 
-        <Box component="h2" className="text-lg flex justify-between items-center">
-          <span>{product.name}</span>
-          {
-            product?.which === 'festival' &&
-            <span className='bg-red-500 rounded-2xl text-center self-center sm:min-w-[50px] min-w-[40px] sm:text-base text-xs p-1 text-white'>
-              {convertToFarsiNumbers(product?.festivalOffPercentage.toString())}%
-            </span>
-          }
-        </Box>
+        <Box className='h-[140px]'>
 
-        <Box className={`p-3 pr-0 ${product?.which !== 'major' && 'mt-3'} flex`}>
-          قیمت
-          <span className="text-red-600 ml-1">:</span>
+          <Box component="h2" className="text-lg flex justify-between items-center">
+            <span className={`${product.name.length > 20 && 'text-sm'}`}>{product.name}</span>
+            {
+              product?.which === 'festival' &&
+              <span className='bg-red-500 rounded-2xl text-center self-center sm:min-w-[50px] min-w-[40px] sm:text-base text-xs p-1 text-white'>
+                {convertToFarsiNumbers(product?.festivalOffPercentage.toString())}%
+              </span>
+            }
+          </Box>
 
-          {
-            product?.which === 'festival' ?
-              <span className="sm:text-base text-sm flex flex-col">
-                <span>
-                  {convertToFarsiNumbers(
-                    formatPrice(Math.floor((product.price * (1 - product.festivalOffPercentage / 100))).toString())
-                  )}
+          <Box className={`p-3 pr-0 ${product?.which !== 'major' && 'mt-3'} flex`}>
+            قیمت
+            <span className="text-red-600 ml-1">:</span>
+
+            {
+              product?.which === 'festival' ?
+                <span className="sm:text-base text-sm flex flex-col">
+                  <span>
+                    {convertToFarsiNumbers(
+                      formatPrice(Math.floor((product.price * (1 - product.festivalOffPercentage / 100))).toString())
+                    )}
+                  </span>
+                  <span className="line-through text-red-600">
+                    {convertToFarsiNumbers(
+                      formatPrice(product.price.toString())
+                    )}
+                  </span>
                 </span>
-                <span className="line-through text-red-600">
+                :
+                <span>
                   {convertToFarsiNumbers(
                     formatPrice(product.price.toString())
                   )}
                 </span>
-              </span>
-              :
-              <span>
-                {convertToFarsiNumbers(
-                  formatPrice(product.price.toString())
-                )}
-              </span>
-          }
-          <span className="mr-1">
-            تومان
-          </span>
-        </Box>
-
-        {
-          product.which === 'major' &&
-          <Box className='text-cyan-500 text-xs'>
-            با خرید حداقل {convertToFarsiNumbers(product.quantity)} عدد، {convertToFarsiNumbers(product.majorOffPercentage)}% تخفیف بگیر!
+            }
+            <span className="mr-1">
+              تومان
+            </span>
           </Box>
 
-        }
+          {
+            product.which === 'major' &&
+            <Box className='text-cyan-500 text-xs'>
+              با خرید حداقل {convertToFarsiNumbers(product.quantity)} عدد، {convertToFarsiNumbers(product.majorOffPercentage)}% تخفیف بگیر!
+            </Box>
 
-        <Box className='absolute bottom-1 min-h-[120px]'>
+          }
+
+        </Box>
+
+        <Box>
           <Box className="my-3 text-center">
             <AddButton productId={product._id} which={product.which} quantity={product?.quantity} profit={product.price * product?.majorOffPercentage / 100} sellerId={product.sellerId} />
           </Box>
