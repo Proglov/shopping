27c37575c40c1c74bd777abc347258cbfffb2fcb
Proglov <dev.mohammad.parvizi@gmail.com>
2024-11-02 16:53:52 +0330

    updated citiesModal and added cookies

--- src/components/home/CitiesModal.jsx
+++ src/components/home/CitiesModal.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useContext, useEffect, useState } from 'react';
 import Backdrop from '@mui/material/Backdrop';
 import Box from '@mui/material/Box';
@@ -9,6 +9,7 @@ import { ModalContext } from './CitiesFilter';
 import { MdExpandMore } from 'react-icons/md';
 import Api from '@/services/withAuthActivities/city';
 import { GradientCircularProgress } from '@/app/loading';
+import { getCookie, setCookie } from 'cookies-next';
 
 const ModalStyle = {
     position: 'absolute',
@@ -23,84 +24,89 @@ const ModalStyle = {
 };
 
 export default function CitiesModal() {
-    const { isModalOpen, setIsModalOpen } = useContext(ModalContext)
-    const [provinces, setProvinces] = useState([])
-    const [isLoading, setIsLoading] = useState(false)
+    const { isModalOpen, setIsModalOpen } = useContext(ModalContext);
+    const cookie = getCookie('cityIds');
+    const preSelectedCities = !!cookie ? JSON.parse(cookie) : [];
+
+    const [provinces, setProvinces] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
 
     useEffect(() => {
-        const getProvinces = async () => {
-            const { getAllCities } = Api
-            const response = await getAllCities()
-            const allProvinces = response.cities.reduce((acc, curr) => {
-                const existingProvince = acc.find(item => item.provinceId === curr.provinceId._id);
+        if (isModalOpen) {
+            fetchProvinces();
+        }
+    }, [isModalOpen]);
 
-                if (existingProvince) {
-                    existingProvince.cities.push({
+    const fetchProvinces = async () => {
+        const { getAllCities } = Api;
+        const response = await getAllCities();
+        const allProvinces = response.cities.reduce((acc, curr) => {
+            const existingProvince = acc.find(item => item.provinceId === curr.provinceId._id);
+
+            if (existingProvince) {
+                existingProvince.cities.push({
+                    cityId: curr._id,
+                    cityName: curr.name,
+                    isSelected: preSelectedCities.includes(curr._id),
+                });
+            } else {
+                acc.push({
+                    provinceId: curr.provinceId._id,
+                    provinceName: curr.provinceId.name,
+                    cities: [{
                         cityId: curr._id,
                         cityName: curr.name,
-                        isSelected: false
-                    });
-                } else {
-                    acc.push({
-                        provinceId: curr.provinceId._id,
-                        provinceName: curr.provinceId.name,
-                        cities: [{
-                            cityId: curr._id,
-                            cityName: curr.name,
-                            isSelected: false
-                        }]
-                    });
-                }
+                        isSelected: preSelectedCities.includes(curr._id),
+                    }],
+                });
+            }
 
-                return acc;
-            }, []);
-            setProvinces(allProvinces)
-        }
-        getProvinces()
-    }, [])
+            return acc;
+        }, []);
+        setProvinces(allProvinces);
+    };
 
-
-    const handleClose = () => setIsModalOpen(false)
+    const handleClose = () => setIsModalOpen(false);
 
     const handleConfirm = () => {
-        setIsLoading(true)
+        setIsLoading(true);
+        const cityIds = [];
 
-        const cityIds = []
+        provinces.forEach(province => {
+            province.cities.forEach(city => {
+                if (city.isSelected) {
+                    cityIds.push(city.cityId);
+                }
+            });
+        });
 
-        provinces.map(province => province.cities.map(city => {
-            if (city.isSelected) cityIds.push(city.cityId)
-        }))
-
-        setIsModalOpen(false)
-        setIsLoading(false)
-    }
+        setCookie('cityIds', cityIds, { maxAge: 3600, path: '/', sameSite: 'strict', secure: true });
+        setIsModalOpen(false);
+        setIsLoading(false);
+        location.reload();
+    };
 
     const handleCheckboxChange = (provinceId, cityId) => {
         setProvinces(prev => {
-            const newState = JSON.parse(JSON.stringify(prev))
-            const provinceIndex = newState.findIndex(province => province.provinceId === provinceId)
-            const cityIndex = newState[provinceIndex].cities.findIndex(city => city.cityId === cityId)
-            newState[provinceIndex].cities[cityIndex].isSelected = !newState[provinceIndex].cities[cityIndex].isSelected
-            return newState
-        })
+            const newState = JSON.parse(JSON.stringify(prev));
+            const provinceIndex = newState.findIndex(province => province.provinceId === provinceId);
+            const cityIndex = newState[provinceIndex].cities.findIndex(city => city.cityId === cityId);
+            newState[provinceIndex].cities[cityIndex].isSelected = !newState[provinceIndex].cities[cityIndex].isSelected;
+            return newState;
+        });
     };
 
-    const isAllCitiesSelected = cities => {
-        for (let i = 0; i < cities.length; i++) {
-            if (!cities[i].isSelected) return false
-        }
-        return true
-    }
+    const isAllCitiesSelected = cities => cities.every(city => city.isSelected);
 
-    const handleAllCitiesCheckboxChange = (provinceId) => {
+    const handleAllCitiesCheckboxChange = provinceId => {
         setProvinces(prev => {
-            const newState = JSON.parse(JSON.stringify(prev))
-            const provinceIndex = newState.findIndex(province => province.provinceId === provinceId)
-            const bool = !isAllCitiesSelected(newState[provinceIndex].cities)
-            newState[provinceIndex].cities.forEach(city => city.isSelected = bool);
-            return newState
-        })
+            const newState = JSON.parse(JSON.stringify(prev));
+            const provinceIndex = newState.findIndex(province => province.provinceId === provinceId);
+            const bool = !isAllCitiesSelected(newState[provinceIndex].cities);
+            newState[provinceIndex].cities.forEach(city => (city.isSelected = bool));
+            return newState;
+        });
     };
 
     return (
@@ -119,61 +125,64 @@ export default function CitiesModal() {
         >
             <Fade in={isModalOpen}>
                 <Box sx={ModalStyle} className='rounded-3xl'>
-
                     <div className='text-center w-full mb-3 text-cyan-700'>
                         شهرهای مورد نظرتان را انتخاب کنید
                     </div>
-
-                    {
-                        provinces.map(((provinceObj, i) => (
-                            <Accordion key={i * 223 + 5}>
-                                <AccordionSummary
-                                    expandIcon={<MdExpandMore />}
-                                    aria-controls="panel1-content"
-                                    id={"panel-header" + i}
-                                >
-                                    <Typography>{provinceObj.provinceName}</Typography>
-                                </AccordionSummary>
-                                <AccordionDetails>
-                                    <Typography className='flex justify-center'>
-                                        <FormControlLabel dir='ltr' control={<Checkbox
+                    {provinces.map((provinceObj, i) => (
+                        <Accordion key={i * 223 + 5}>
+                            <AccordionSummary
+                                expandIcon={<MdExpandMore />}
+                                aria-controls="panel1-content"
+                                id={"panel-header" + i}
+                            >
+                                <Typography>{provinceObj.provinceName}</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <Typography className='flex justify-center'>
+                                    <FormControlLabel
+                                        dir='ltr'
+                                        control={<Checkbox
                                             color='info'
                                             checked={isAllCitiesSelected(provinceObj.cities)}
                                             onChange={() => handleAllCitiesCheckboxChange(provinceObj.provinceId)}
-                                        />} label={'همه ی ' + provinceObj.provinceName} />
-                                    </Typography>
-                                    <Typography className='flex flex-col items-center'>
-                                        {
-                                            provinceObj.cities.map(cityObj => (
-                                                <FormControlLabel key={cityObj.cityId} dir='ltr' control={<Checkbox checked={cityObj.isSelected} onChange={() => handleCheckboxChange(provinceObj.provinceId, cityObj.cityId)}
-                                                />} label={cityObj.cityName} />
-                                            ))
-                                        }
-                                    </Typography>
-                                </AccordionDetails>
-                            </Accordion>
-                        )))
-                    }
-
+                                        />}
+                                        label={'همه ی ' + provinceObj.provinceName}
+                                    />
+                                </Typography>
+                                <Typography className='flex flex-col items-center'>
+                                    {provinceObj.cities.map(cityObj => (
+                                        <FormControlLabel
+                                            key={cityObj.cityId}
+                                            dir='ltr'
+                                            control={<Checkbox
+                                                checked={cityObj.isSelected}
+                                                onChange={() => handleCheckboxChange(provinceObj.provinceId, cityObj.cityId)}
+                                            />}
+                                            label={cityObj.cityName}
+                                        />
+                                    ))}
+                                </Typography>
+                            </AccordionDetails>
+                        </Accordion>
+                    ))}
                     <div className='mt-6 flex justify-between'>
-                        {
-                            isLoading ?
-                                <div className='w-full text-center'>
-                                    <GradientCircularProgress />
-                                </div>
-                                :
-                                <>
-                                    <Button onClick={handleConfirm} variant='outlined'
-                                        className='p-0 m-1'
-                                        sx={{ color: 'green', borderColor: 'green' }}>
-                                        تایید
-                                    </Button>
-                                    <Button onClick={handleClose} variant='outlined'
-                                        className='p-0 m-1'
-                                        sx={{ color: 'red', borderColor: 'red' }}>
-                                        برگشت
-                                    </Button>
-                                </>
+                        {isLoading ?
+                            <div className='w-full text-center'>
+                                <GradientCircularProgress />
+                            </div>
+                            :
+                            <>
+                                <Button onClick={handleConfirm} variant='outlined'
+                                    className='p-0 m-1'
+                                    sx={{ color: 'green', borderColor: 'green' }}>
+                                    تایید
+                                </Button>
+                                <Button onClick={handleClose} variant='outlined'
+                                    className='p-0 m-1'
+                                    sx={{ color: 'red', borderColor: 'red' }}>
+                                    برگشت
+                                </Button>
+                            </>
                         }
                     </div>
                 </Box>
--- src/lib/axios.js
+++ src/lib/axios.js
@@ -6,22 +6,20 @@ const apiURL = process.env.NEXT_PUBLIC_BackEnd_API;
 export const clientWithoutAuth = axios.create({
     baseURL: `${apiURL}`,
     timeout: 30000,
-    withCredentials: false,
+    withCredentials: true,
     headers: {
         "X-Client-Name": "browser",
         "Accept": "application/json",
-        "Access-Control-Allow-Origin": "*"
     }
 });
 
 export const clientWithAuth = axios.create({
     baseURL: `${apiURL}`,
     timeout: 30000,
-    withCredentials: false,
+    withCredentials: true,
     headers: {
         "X-Client-Name": "browser",
         "Accept": "application/json",
-        "Access-Control-Allow-Origin": "*",
         "Authorization": typeof window !== "undefined" ? localStorage.getItem('token') ?? null : null
     }
 });
