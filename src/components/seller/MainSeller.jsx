844758e9288973dbfc5a0c07acdd2977c0890898
Proglov <dev.mohammad.parvizi@gmail.com>
2024-11-24 16:43:25 +0330

    fixed Component.name issue

--- src/components/seller/MainSeller.jsx
+++ src/components/seller/MainSeller.jsx
@@ -68,7 +68,7 @@ const CustomTabs = ({ array, handleChange, tabs, discountTabs }) => (
                     const props = {
                         which: "Seller"
                     }
-                    if (Component.name === 'Discounts') props.tabs = discountTabs
+                    props.tabs = discountTabs
                     return (
                         <TabPanel key={index + 200} value={tabs.active} index={index} className='text-center'>
                             {<Component {...props} />}
